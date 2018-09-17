8726127424008ff65e02935baae4262846ff1175
Necake <neca1mesto@gmail.com>
2018-09-17 13:40:06 +0200

    Added player controls

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    int playerLane;
+    public float moveSpeed;
+    public float laneSize = 2.0f;
+    Vector3 dest;
+	// Use this for initialization
+	void Start () {
+        playerLane = 1; //ranges from 0 to 2
+        dest = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.A) && playerLane > 0)
+        {//ide levo ako ima gde
+            playerLane--;
+            dest += new Vector3(-laneSize, 0, 0);
+        }
+        else if(Input.GetKeyDown(KeyCode.D) && playerLane < 2)
+        {//ide desno ako ima gde
+            playerLane++;
+            dest += new Vector3(laneSize, 0, 0);
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, dest, Time.deltaTime * moveSpeed);
+    }
+
+}
